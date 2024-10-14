3dabe7944cb89679347c48df99e83af961877c40
Yehor Nazarkin <ynazarkin@healthjoy.com>
2024-10-15 01:31:19 +0300

    dropped linux-arm support

--- install_actions.sh
+++ install_actions.sh
@@ -6,9 +6,6 @@ export TARGET_ARCH="x64"
 if [[ $TARGETPLATFORM == "linux/arm64" ]]; then
   export TARGET_ARCH="arm64"
 fi
-if [[ $TARGETPLATFORM == "linux/arm" ]]; then
-  export TARGET_ARCH="arm"
-fi
 
 curl -L "https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-${TARGET_ARCH}-${GH_RUNNER_VERSION}.tar.gz" > actions.tar.gz
 tar -zxf actions.tar.gz
